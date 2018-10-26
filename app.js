feef62b54a427a030b1dc758f41f16949ce89310
Daniel Phonelath <dphonelath@Daniels-MacBook-Pro.local>
2018-10-26 10:48:23 -0600

    wired routes to app.js, export modules, added middleware

--- app.js
+++ app.js
@@ -5,5 +5,25 @@ const parser = require('body-parser')
 
 let port = process.env.PORT || 3000
 
-const cakes = ('./cakes.json')
-const students = ('./students.json')
+
+// Routes Imports
+const cakesRoutes = require('./routes/cakesRoutes')
+const studentsRoutes = ('./routes/studentRoutes')
+
+//middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded(({extended:false})))
+app.use(cors())
+
+// Any requests that START with /cakes or /students, send to this router file
+app.use('/cakes', cakesRoutes)
+app.use('/students', studentsRoutes)
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Server running on ${port}`))
+
