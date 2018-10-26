9eec87c9c4869113d5010ab2e5b2256aab4c2c3e
Daniel Phonelath <dphonelath@Daniels-MacBook-Pro.local>
2018-10-26 11:37:41 -0600

    added error handling, and router get requests on cakes, NEED to debug error handling.

--- app.js
+++ app.js
@@ -19,12 +19,32 @@ app.use(cors())
 app.use('/cakes', cakesRoutes)
 app.use('/students', studentsRoutes)
 
+// Checks if server is working
 app.get('/', (req,res) =>{
   res.send('Yooooooo Dawg')
 })
 
+// Error handling
+app.use(notFound)
+app.use(errorHandler)
 
+function notFound(req, res, next) {
+    res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
+}
+  
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
 
-
+    res.status(500).send({
+      error: err.message,
+      stack,
+      url: req.originalURL
+    })
+  
+    res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+  }
+  
 
 app.listen(port, () => console.log(`Server running on ${port}`))
--- routes/cakesRoutes.js
+++ routes/cakesRoutes.js
@@ -3,8 +3,24 @@ const router = express.Router()
 
 const cakes = require('../cakes.json')
 
+// grabs data from JSON package
 router.get('/', (req,res) => {
   res.json({cakes:cakes})
+})
+
+// grabbing IDs from JSON
+router.get('/:id', (req,res,next) => {
+    const id = req.params.id
+
+    const cake = cakes.filter(cake => {
+        return cake.id == id
+    })
+    console.log('cake variable length', cake.length)
+    if(!cake.length){
+        next()
+    }
+
+    res.json({cake:cake[0]})
 })
 
 
